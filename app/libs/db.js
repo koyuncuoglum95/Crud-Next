21a3a6b2a68d843e75c8b148d097b935a4db33df
Mehmet Ali Koyuncuoglu <77508734+koyuncuoglum95@users.noreply.github.com>
2023-07-28 14:13:06 -0700

    Update db.js

--- app/libs/db.js
+++ app/libs/db.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
 
 export default prisma
